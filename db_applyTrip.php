58e6a534a0f3f71418d6472a06e45d62e2edd3e3
MrMo-ux <68288408+MrMo-ux@users.noreply.github.com>
2021-03-30 01:54:56 +0800

    Update db_applyTrip.php

--- db_applyTrip.php
+++ db_applyTrip.php
@@ -2,7 +2,7 @@
 $servername='localhost';
 $username='root';
 $password='';
-$dbname = "db_projectresq";
+$dbname = "applytrips";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
    die('Could not Connect My Sql:' .mysql_error());
